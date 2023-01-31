aa24058c48384c691de08154e1f3628917e4063d
hoanb24 <115441410+hoanb24@users.noreply.github.com>
2023-01-31 11:34:36 +0700

    Add files via upload

---
+++ ifelse.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>My first PHP page</h1>
+<?php
+$mung = "Visit honey";
+$mung2 = "Visit Papa";
+$mung3 = "Visit Mama";
+$mung4 = "Visit Bo";
+$mung7 = "I'm learing at PNV";
+if ($mung == $mung7){
+    echo 'True';
+}
+else {
+    echo 'False';
+}
+?>
+
+</body>
+</html>
---
+++ switch.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>My first PHP page</h1>
+<?php
+$mong = "Visit honey";
+$mong2 = "Visit Papa";
+$mong3 = "Visit Mama";
+$mong4 = "Visit Bo";
+$mong7 = "I'm learing at PNV";
+switch ($mong) {
+    case $mong2 :
+        echo 'Hello Papa';
+        break;
+    case  $mong3:
+        echo 'Hello Mama';
+        break;
+    case $mong4 :
+        echo 'Hello Homie';
+        break;
+    case $mong7 :
+        echo 'Hey Girl :3';
+        break;
+    }
+?>
+
+</body>
+</html>
